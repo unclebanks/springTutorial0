48d5368d71d204611435299a55f67b345c3ad5aa
unclebanks <49108377+unclebanks@users.noreply.github.com>
2021-11-10 20:28:35 -0800

    Initial Commit
    * Blank template provided by Spring website

---
+++ src/main/java/com/example/Spring0Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring0Application.class, args);
+	}
+
+}
